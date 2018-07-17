3c002c6ca8d80d719ddb2a3363c7568a22e918ea
mvpache <mvpache@msn.com>
2018-07-17 13:04:51 -0700

    Redux folder structure

---
+++ client/src/actions/actionTypes.js
@@ -0,0 +1,18 @@
+const actionTypes = {
+  LOGIN_EMPLOYER: {},
+  LOGOUT_EMPLOYER: {},
+  REGISTER_EMPLOYER: {},
+};
+
+Object.keys(actionTypes).forEach(type => {
+  actionTypes[type].SUCCESS = `${type}_SUCCESS`;
+  actionTypes[type].ERROR = `${type}_ERROR`;
+  actionTypes[type].IN_PROGRESS = `${type}_IN_PROGRESS`;
+});
+
+actionTypes.LOGIN.SHOW_MODAL = 'LOGIN_SHOW_MODAL';
+actionTypes.LOGIN.HIDE_MODAL = 'LOGIN_HIDE_MODAL';
+actionTypes.REGISTER.SHOW_MODAL = 'REGISTER_SHOW_MODAL';
+actionTypes.REGISTER.HIDE_MODAL = 'REGISTER_HIDE_MODAL';
+
+export default actionTypes;
