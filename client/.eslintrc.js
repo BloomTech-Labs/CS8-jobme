68502ff1eb3e0143847efb1196c9c1f58ac48942
mvpache <mvpache@msn.com>
2018-07-20 09:04:11 -0700

    added heroku url to actions and cors

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
     "extends": "airbnb-base",
-    "parser": "babel-eslint",
     "rules": {
       "no-unused-vars": 0,
       "line-break-style": 0,
--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 import actionTypes from './actionTypes';
 
-const url = process.env.NODE_ENV === 'production' ? 'heroku' : 'http://localhost:5000/api';
+const url = process.env.NODE_ENV === 'production'
+  ? 'http://jobitduder.herokuapp.com/api'
+  : 'http://localhost:5000/api';
 
 axios.defaults.baseURL = url;
 
