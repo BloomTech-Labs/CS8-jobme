97c2e64580b87733fa02c6e545ab99f56ffdb7be
mvpache <mvpache@msn.com>
2018-07-23 10:36:24 -0700

    added login seeker reducer and state

--- client/src/reducers/loggedInJobSeeker.js
+++ client/src/reducers/loggedInJobSeeker.js
@@ -1,2 +1,29 @@
-// open loggedInEmployer reducer side be side, they will be very
-// similar save for naming of actions/main section state
+import actionTypes from '../actions/actionTypes';
+
+const defaultState = {
+  token: '', // use token to test if logged in, == type coercion
+  profile: {
+    firstName: '',
+    lastName: '',
+    industry: '',
+    email: '',
+    summary: '',
+    desiredTitle: '',
+    topSkills: [''],
+    additionalSkills: '',
+    familiarWith: '',
+  },
+};
+
+export default (state = defaultState, action) => {
+  switch (action.type) {
+    case actionTypes.LOGIN_SEEKER.SUCCESS:
+      const { token } = action;
+      return { ...state, token };
+    case actionTypes.GET_SEEKER_PROFILE.SUCCESS:
+      const { profile } = action;
+      return { ...state, profile };
+    default:
+      return state;
+  }
+};
