312a80c27a9fc442a6d71e8e2da3b162d54844ab
Juber Ahmed <a.juber@hotmail.com>
2018-07-24 16:05:42 -0400

    Added routes for employer and job seeker register

---
+++ client/src/containers/JobSeekerRegister.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { SeekerRegister } from '../components';
+
+class DisplaySeekerRegister extends Component {
+    render() {
+        return (
+            <div>
+                <SeekerRegister />  
+            </div>
+        );
+    }
+}
+
+export default DisplaySeekerRegister;
