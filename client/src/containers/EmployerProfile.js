94d38eb396d244c90fe598b0f263614d297ad340
Juber Ahmed <a.juber@hotmail.com>
2018-07-24 16:05:42 -0400

    Added routes for employer and job seeker register

---
+++ client/src/containers/EmployerProfile.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react';
+import { EmployerProfile } from '../components';
+
+const DisplayEmployerProfile = () => {
+  return (
+    <div>
+      <Fragment>
+          <EmployerProfile/>
+      </Fragment>
+    </div>
+  );
+};
+
+export default DisplayEmployerProfile;
---
+++ client/src/containers/JobSeekerProfile.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react';
+import { SeekerProfile } from '../components';
+
+const DisplaySeekerProfile = () => {
+  return (
+    <div>
+      <Fragment>
+          <SeekerProfile/>
+      </Fragment>
+    </div>
+  );
+};
+
+export default DisplaySeekerProfile;
