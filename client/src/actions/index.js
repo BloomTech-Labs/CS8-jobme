759f680638b595a49fd9aa8c7927934c263dbede
Juber Ahmed <a.juber@hotmail.com>
2018-07-24 16:27:49 -0400

    Updating actions and reducers

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,38 +3,15 @@ import {
   getEmployerProfile,
   loginEmployer,
   registerEmployer,
-  updateEmployerProfile,
-  updateEmployerPassword,
 } from './employerActions';
 
 import {
-<<<<<<< HEAD
-  loginSeeker,
-  getSeekerProfile,
-  updateSeekerProfile,
-  updateSeekerPassword,
-} from './seekerActions';
-
-export {
-  loginEmployer,
-  registerEmployer,
-  getEmployerProfile,
-  updateEmployerProfile,
-  updateEmployerPassword,
-  // -----
-  loginSeeker,
-  // registerSeeker,
-  getSeekerProfile,
-  updateSeekerProfile,
-  updateSeekerPassword,
-=======
   getJobSeekerProfile,
   loginJobSeeker,
   registerJobSeeker,
 } from './seekerActions';
 
 export {
-  getEmployerProfile, loginEmployer, registerEmployer, //Employer 
+  getEmployerProfile, loginEmployer, registerEmployer,
   loginJobSeeker, getJobSeekerProfile, registerJobSeeker,
->>>>>>> Added actions/reducers/ui so job seekers can register
 };
