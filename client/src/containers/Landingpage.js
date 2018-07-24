efb40d8c5558671aeaa38059dcb7dcc41a827e60
Kevin Jolley <serrowpls@gmail.com>
2018-07-23 19:17:36 -0500

    landing page ported in, moved old landing to storage folder. Styles are broken, will update

--- client/src/containers/Landingpage.js
+++ client/src/containers/Landingpage.js
@@ -1,48 +1,28 @@
 import React from 'react';
-import Coverflow from 'react-coverflow';
-import { StyleRoot } from 'radium';
 
-import img from '../images/img4.png';
+import img from '../images/hand.jpeg';
+import img2 from '../images/itworks.png';
 
 import './tempcss/landing.css'
 
 const LandingPage = () => {
   return (
-    <div>
-      <div class="land_header">
-        <header style={{ display: 'flex', justifyContent: 'flex-end' }}>
-          <a href="/signup" class="nav_link">Sign up</a>
-          <a href="/login" class="nav_link">Sign in</a>
-        </header>
+    <div class="bkg_img">
+      <img src={img} />
+      <div class="bro_bigtext">
+        <h1> Job Me Bro! </h1>
+        <h4> Our Proven System WORKS!!! </h4>
+        <img src={img2} class="img2" />
       </div>
-      <StyleRoot>
-        <Coverflow
-          displayQuantityOfSide={3}
-          navigation
-          infiniteScroll
-          enableHeading
-          clickable
-          media={{
-            '@media (max-width: 900px)': {
-              width: '600px',
-              height: '300px'
-            },
-            '@media (min-width: 900px)': {
-              width: '960px',
-              height: '600px'
-            }
-          }}
-        >
-          <img src={img} alt='' data-action="" />
-          <img src={img} alt='' data-action="" />
-          <img src={img} alt='' data-action="" />
-          <img src={img} alt='' data-action="" />
-          <img src={img} alt='' data-action="" />
-          <img src={img} alt='' data-action="" />
-          <img src={img} alt='' data-action="" />
-        </Coverflow>
-      </StyleRoot>
-
+      <div class="bro_signup_cont">
+        <div class="bro_signup">
+          <h1> Sign Up Today! </h1>
+          <button class="ripple"> I'm a Seeker </button>
+          <button class="ripple"> I'm a Recruiter </button>
+          <p> Already have an account?</p>
+          <button class="ripple"> Log-In </button>
+        </div>
+      </div>
     </div>
   );
 };
