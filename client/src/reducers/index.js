b83619eee48e841de118abd6fa4ebdc589adb77e
Juber Ahmed <a.juber@hotmail.com>
2018-07-24 15:48:40 -0400

    Added actions/reducers/ui so job seekers can register

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,6 +2,15 @@ import { combineReducers } from 'redux';
 
 import loggedInEmployer from './loggedInEmployer';
 import registerEmployerSuccess from './registerEmployer';
+<<<<<<< HEAD
 import loggedInSeeker from './loggedInSeeker';
 // Just wasted an hour because of this name difference...
 export default combineReducers({ loggedInEmployer, registerEmployerSuccess, loggedInSeeker });
+=======
+import loggedInSeeker from './loggedInJobSeeker';
+import registerJobSeeker from './registerJobSeeker';
+
+export default combineReducers({
+  loggedInEmployer, registerEmployerSuccess, loggedInSeeker, registerJobSeeker,
+});
+>>>>>>> Added actions/reducers/ui so job seekers can register
