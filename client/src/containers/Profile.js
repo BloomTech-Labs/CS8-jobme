793a8f39c834d60cf2bf6d39ce486f899a94777e
mvpache <mvpache@msn.com>
2018-07-24 21:28:50 -0700

    fixed error where seeker sucess state was named wrong

--- client/src/containers/Profile.js
+++ client/src/containers/Profile.js
@@ -3,14 +3,8 @@ import { connect } from 'react-redux';
 import { EmployerProfile, SeekerProfile } from '../components';
 import { getEmployerProfile, getSeekerProfile } from '../actions';
 import { withRouter } from 'react-router-dom';
+
 class Profile extends Component {
-  componentDidMount() {
-    const seekerToken = localStorage.getItem('seekerToken');
-    const employerToken = localStorage.getItem('employerToken');
-
-    seekerToken ? this.props.getEmployerProfile(employerToken) : this.props.getSeekerProfile(seekerToken);
-  }
-
   render() {
     return (
       <div>
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,7 +3,7 @@ import { combineReducers } from 'redux';
 import loggedInEmployer from './loggedInEmployer';
 import registerEmployerSuccess from './registerEmployer';
 import loggedInSeeker from './loggedInSeeker';
-import registerJobSeeker from './registerJobSeeker';
+import registerJobSeekerSucess from './registerJobSeeker';
 
 export default combineReducers({
   loggedInEmployer, registerEmployerSuccess, loggedInSeeker, registerJobSeeker,
