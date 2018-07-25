5a17df9cba275c73ac6949776bf622b222f9dce0
Kevin Jolley <serrowpls@gmail.com>
2018-07-25 15:54:34 -0500

    Navigation adjusted to play nice with Bootstrap styling, removed state and children components from Navigation

--- client/src/containers/nav/Nav.js
+++ client/src/containers/nav/Nav.js
@@ -2,18 +2,12 @@ import React, { Component } from 'react';
 
 import './temp.css';
 
-import NavConst from './NavConst';
-
 class Nav extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      isOpen: false,
-      name: ['Kevin'],
-      postsAva: ['1'],
-      freeCall: ['3'],
-      credits: ['20'],
+      isOpen: true,
     };
 
     this.toggle = this.toggle.bind(this);
@@ -28,38 +22,39 @@ class Nav extends Component {
     const open = this.state.isOpen;
     return (
       <div className="nav_container">
-        <div className="navContainer">
-          <NavConst
-            name={this.state.name}
-            postsAva={this.state.postsAva}
-            freeCall={this.state.freeCall}
-            credits={this.state.credits}
-          />
-          <h4 className={open ? 'collapse_open' : 'collapse'} onClick={this.toggle}>
-            |||
-          </h4>
+        <div className="cont">
+          <div className="navContainer">
+            <h4
+              className={open ? 'collapse_closed' : 'collapse_open'}
+              onClick={this.toggle}
+            >
+              |||
+            </h4>
+          </div>
         </div>
         <div
           className={open ? 'nav_collapse' : 'nav_collapse_open'}
-          onClick={this.toggle} // this toggles the auto-close
+          // onClick={this.toggle} // this toggles the auto-close
         >
-          <a href="/" className="nav_link">
-            <h3> Home </h3>
-          </a>
-          <a href="/profile" className="nav_link">
+          <a href="#" className="navLink">
             <h3> Profile </h3>
           </a>
-          <a href="/matches" className="nav_link">
+          <a href="#" className="navLink">
             <h3> Matches </h3>
           </a>
-          <a href="/browse" className="nav_link">
-            <h3> Job Postings </h3>
+          <a href="#" className="navLink">
+            <h3> Messages </h3>
           </a>
-          <a href="/billing" className="nav_link">
+
+          <a href="#" className="navLink">
+            <h3>Job Postings </h3>
+          </a>
+          <a href="#" className="navLink">
             <h3> Billing </h3>
           </a>
+
           <hr />
-          <a href="/login" className="nav_link">
+          <a href="#" className="navLink">
             <h3> Sign Out </h3>
           </a>
         </div>
