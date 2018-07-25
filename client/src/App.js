fa4e1092a95ebdea61cd21a68a3019a73a1c4db4
William <wtpwinberg@gmail.com>
2018-07-25 14:10:08 -0700

    CreditsInfo component made and App ajusted accordingly

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,29 @@ import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
 
 import Nav from './containers/nav/Nav';
+import CreditsInfo from './containers/nav/CreditsInfo';
 import Body from './containers/Body';
 import { getEmployerProfile, getSeekerProfile } from './actions';
+
+import styled from 'styled-components';
+const Container = styled.div`
+  min-width: 800px;
+  max-width: 1400px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 800px;
+  width: 100%;
+`;
+const Menu= styled.div`
+  position: fixed;
+  top: 1em;
+  right: 1em;
+`;
 
 class App extends Component {
   // eventually we want a listner/action that checks
@@ -14,10 +35,18 @@ class App extends Component {
   // access a protected route
 
   render() {
-    return <div>
-      {localStorage.getItem('employerToken') || localStorage.getItem('seekerToken') ? <Nav /> : <Fragment />}
-        <Route path="/" component={Body} />
-      </div>;
+    return (
+      <Container>
+        {localStorage.getItem('employerToken') ||
+         localStorage.getItem('seekerToken') ?
+        <Content>
+          <CreditsInfo/>
+            <Route path="/" component={Body} />
+            <Menu><Nav/></Menu>
+        </Content> : 
+        <Fragment />}
+      </Container>
+    );
   }
 }
 
