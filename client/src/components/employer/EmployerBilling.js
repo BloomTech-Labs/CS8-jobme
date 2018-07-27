b96632f99b40d714ea0b3e8d72e23c63e5a65214
runranron <williamrpelton@gmail.com>
2018-07-27 11:52:19 -0500

    did it

--- client/src/components/employer/EmployerBilling.js
+++ client/src/components/employer/EmployerBilling.js
@@ -3,7 +3,7 @@ import BillingForm  from './BillingForm';
 
 import {StripeProvider} from 'react-stripe-elements';
 import { Elements } from  'react-stripe-elements';
-const stripePublic = process.env.STRIPE_PUBLIC;
+const stripePublic = process.env.REACT_APP_STRIPE_PUBLIC;
 
 
 class EmployerBilling extends Component {
