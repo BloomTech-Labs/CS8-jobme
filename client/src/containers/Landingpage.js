564bc87950bdde5385bb19021e2b873365a745cc
William <wtpwinberg@gmail.com>
2018-07-29 11:27:27 -0700

    LandingPage

--- client/src/containers/Landingpage.js
+++ client/src/containers/Landingpage.js
@@ -10,15 +10,15 @@ const LandingPage = () => {
   return <div className="bkg_img">
       <img src={img} alt="Find your job" className="img1" />
       <div className="bro_bigtext">
-        <h1> Job Me Bro! </h1>
+        <h1>Job Me Bro!</h1>
       </div>
       <div className="bro_signup_cont">
         <div className="bro_signup">
           <h1> Sign Up Today! </h1>
-          <Link to={{ pathname: '/signup', state: { seekerRegister: true } }}>
+          <Link to={{ pathname: '/signup', state: { seekerRegister: true }}}>
             <button className="ripple"> I'm a Seeker </button>
           </Link>
-          <Link to={{ pathname: '/signup', state: { seekerRegister: false } }}>
+          <Link to={{ pathname: '/signup', state: { seekerRegister: false }}}>
             <button className="ripple"> I'm a Recruiter </button>
           </Link>
           <p> Already have an account?</p>
