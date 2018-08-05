d29b952fcb629625d892cfed40d7f496c63e6aba
William <wtpwinberg@gmail.com>
2018-08-04 17:36:46 -0700

    PostedJobs delete working

--- client/src/containers/PostedJobs.js
+++ client/src/containers/PostedJobs.js
@@ -3,7 +3,7 @@ import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Progress from './Progress';
-import { getJobs } from '../actions';
+import { getJobs, deleteJob } from '../actions';
 
 import {
   GridContainer,
@@ -21,36 +21,46 @@ class PostedJobs extends Component {
     this.props.getJobs();
   }
 
+  delete = (index, event) => {
+    const id = this.props.availableJobs[index]._id;
+    this.props.deleteJob(id);
+  }
 
   render() {
-    if (!this.props.availableJobs) return <Progress/>;
+    if (!this.props.availableJobs) return <Progress />;
     const { availableJobs } = this.props;
 
-
     return (
-    <GridContainer>
-      {availableJobs.map((job, i) => {
-        return (
-          <Card key={`${job.titleAndSalary}${i}`}>
-            <Link to={{ pathname: `/jobs/${i}` }}>
-              <CardHeader>
-                <CardName>
-                  {job.titleAndSalary}
-                </CardName>
-              </CardHeader>
-            </Link>
-            <Paragraph>{job.description}</Paragraph>
-            <ButtonsContainer>
-              <CardButton edit
-                onClick={() => this.props.history.push(`/jobs/${i}`)}
+      <GridContainer>
+        {availableJobs.map((job, i) => {
+          return (
+            <Card key={`${job.titleAndSalary}${i}`}>
+              <Link to={{ pathname: `/jobs/${i}` }}>
+                <CardHeader>
+                  <CardName>
+                    {job.titleAndSalary}
+                  </CardName>
+                </CardHeader>
+              </Link>
+              <Paragraph>{job.description}</Paragraph>
+              <ButtonsContainer>
+                <CardButton edit
+                  onClick={() => this.props.history.push(`/jobs/${i}`)}
                 />
-              <CardButton trash/>
-            </ButtonsContainer>
-          </Card>
-        )
-      })}
-    </GridContainer>
+                <CardButton trash
+                  onClick={this.delete.bind(this, i)} />
+              </ButtonsContainer>
+            </Card>
+          )
+        })}
+      </GridContainer>
     );
+  }
+  componentDidUpdate(prevProps) {
+
+    if (prevProps.availableJobs.length !== this.props.availableJobs.length) {
+      this.props.getJobs();
+    }
   }
 }
 
@@ -58,4 +68,4 @@ const mapStateToProps = state => ({
   availableJobs: state.jobs.availableJobs,
 });
 
-export default withRouter(connect(mapStateToProps, { getJobs })(PostedJobs));
+export default withRouter(connect(mapStateToProps, { getJobs, deleteJob })(PostedJobs));
