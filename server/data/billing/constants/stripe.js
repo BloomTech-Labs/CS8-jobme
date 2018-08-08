65b6683baed5dc80b4c98cd5863bef26dbd60eec
runranron <williamrpelton@gmail.com>
2018-08-07 21:04:34 -0500

    refactored stripe on backend

--- server/data/billing/constants/stripe.js
+++ server/data/billing/constants/stripe.js
@@ -1,11 +1,5 @@
 const configureStripe = require('stripe');
 
-const testSecret = process.env.STRIPE_PRIVATE;
-
-const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
-  ? 'sk_live_MY_SECRET_KEY'
-  : testSecret;
-
-const stripe = configureStripe(STRIPE_SECRET_KEY);
+const stripe = configureStripe(process.env.STRIPE_PRIVATE);
 
 module.exports = stripe;
