8cd796f82cbafbb6ed1ebb01cbd73431f46e9972
Kevin Jolley <serrowpls@gmail.com>
2018-08-09 17:51:15 -0500

    init

--- client/src/components/styles/globalStyles.js
+++ client/src/components/styles/globalStyles.js
@@ -96,6 +96,7 @@ export const Button = styled.button`
   box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
   &:hover {
     background-color: white;
+    cursor: pointer;
   }
   &:disabled {
     opacity: .5;
