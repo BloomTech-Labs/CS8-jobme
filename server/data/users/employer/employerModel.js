96cc0b0093676b33a03d053babf14def22173897
runranron <williamrpelton@gmail.com>
2018-08-09 18:23:05 -0500

    changed schema

--- server/data/users/employer/employerModel.js
+++ server/data/users/employer/employerModel.js
@@ -8,7 +8,7 @@ const EmployerSchema = new mongoose.Schema({
   companyName: { type: String, required: true },
   companyUrl: { type: mongoose.SchemaTypes.Url, required: true },
   industry: { type: String },
-  description: { type: String, required: true },
+  description: { type: String, required: true, maxlength: 256 },
   // TODO: ADD PASSWORD MIN-LENGTH BEFORE FINAL DEPLOY
   password: { type: String, minlength: 8, required: true },
   email: { type: String, required: true, unique: true },
